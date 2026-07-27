e65b1f17fb7586abae7b308d3258c1c28cf173ab
khushalpatidar00 <khushalpatidar735@gmail.com>
2026-07-27 16:38:10 +0530

    Time: 26 ms (55.72%), Space: 25.3 MB (44.72%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -6,12 +6,13 @@ public:
         for(int i=0;i<s.size();i++){
             mp[s[i]]++;
         }
-         for(int i=0;i<s.size();i++){
-            if(mp[s[i]]>0){
-                cnt++;
-                mp[s[i]]=0;
-            }
-        }
-        return cnt;
+        //  for(int i=0;i<s.size();i++){
+        //     if(mp[s[i]]>0){
+        //         cnt++;
+        //         mp[s[i]]=0;
+        //     }
+        // }
+        // return cnt;
+        return mp.size();
     }
 };
